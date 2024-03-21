08c73547f89eb446a8569f20371b82dc6dd7c237
tcaish <77754475+tcaish@users.noreply.github.com>
2024-03-21 14:16:39 -0400

    added cool bg animation

--- components/AnimatedBackground/index.tsx
+++ components/AnimatedBackground/index.tsx
@@ -1,24 +1,21 @@
 import { useColorMode } from '@chakra-ui/color-mode';
-import './index.scss';
+import DrifterStars from '@devil7softwares/react-drifter-stars';
 
 export default function AnimatedBackground() {
   const { colorMode } = useColorMode();
 
   const styles = {
     main: {
-      backgroundImage:
-        colorMode === 'dark'
-          ? 'linear-gradient(-45deg, #0a2647, #144272, #205295, #2c74b3)'
-          : 'linear-gradient(-45deg, #f7fbfc, #d6e6f2, #b9d7ea, #769fcd)',
-      backgroundSize: '400% 400%',
-      height: '100%',
+      opacity: colorMode === 'dark' ? 0.3 : 1,
       position: 'absolute',
-      width: '100%',
       zIndex: '-1'
     }
   };
 
   return (
-    <div id="animated-background" style={styles.main as React.CSSProperties} />
+    <DrifterStars
+      color={colorMode === 'dark' ? 'white' : '#0293D5'}
+      style={styles.main as React.CSSProperties}
+    />
   );
 }
