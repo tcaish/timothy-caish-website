27f12395d0e2958722f2144f7390996ca0fdd82a
tcaish <77754475+tcaish@users.noreply.github.com>
2024-03-22 07:32:25 -0400

    added cta buttons

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,11 @@ import FullPageBlur from '@/components/FullPageBlur';
 import Navbar from '@/components/Navbar';
 import { useStore } from '@/zustand/store';
 import { useColorMode } from '@chakra-ui/color-mode';
-import { Box, Center, Flex, Heading, Text } from '@chakra-ui/layout';
-import { Avatar } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Stack, Text } from '@chakra-ui/layout';
+import { Avatar, Button } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import Lottie from 'lottie-react';
+import { CircleUserRound, GalleryVerticalEnd } from 'lucide-react';
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { Balancer } from 'react-wrap-balancer';
@@ -56,12 +58,14 @@ export default function Home() {
           <Center flexBasis={0} flexGrow={1} zIndex={0}>
             <FadeIn>
               <Box textAlign={{ base: 'center', lg: 'end' }}>
+                {/* Avatar image */}
                 <Avatar
                   name="Timothy Caish"
                   size="2xl"
                   src="/assets/avatar.png"
                 />
 
+                {/* Greeting */}
                 <Balancer>
                   <Heading
                     color={colorMode === 'dark' ? 'white' : 'gray.700'}
@@ -71,6 +75,7 @@ export default function Home() {
                   </Heading>
                 </Balancer>
 
+                {/* Type animation */}
                 <Balancer>
                   <Heading color={colorMode === 'dark' ? 'white' : 'gray.700'}>
                     <TypeAnimation
@@ -104,7 +109,9 @@ export default function Home() {
                   </Heading>
                 </Balancer>
 
+                {/* Description and CTA */}
                 <Box
+                  mt={4}
                   mx={{ base: 4, sm: 10, lg: 0 }}
                   textAlign={{ base: 'center', lg: 'end' }}
                 >
@@ -112,7 +119,6 @@ export default function Home() {
                     <Text
                       color={colorMode === 'dark' ? 'gray.300' : 'gray.700'}
                       fontSize={{ base: 'md', md: 'lg' }}
-                      mt={4}
                     >
                       With a blend of creativity and technical expertise, I
                       excel as a full stack developer, transforming ideas into
@@ -120,6 +126,33 @@ export default function Home() {
                       engagement and propel businesses forward.
                     </Text>
                   </Balancer>
+
+                  <Stack
+                    direction="row"
+                    justifyContent={{ base: 'center', lg: 'end' }}
+                    mt={4}
+                    spacing={4}
+                  >
+                    <motion.div whileTap={{ scale: 0.8 }}>
+                      <Button
+                        leftIcon={<GalleryVerticalEnd />}
+                        size={{ base: 'md', lg: 'lg' }}
+                        variant="solid"
+                      >
+                        Portfolio
+                      </Button>
+                    </motion.div>
+
+                    <motion.div whileTap={{ scale: 0.8 }}>
+                      <Button
+                        rightIcon={<CircleUserRound />}
+                        size={{ base: 'md', lg: 'lg' }}
+                        variant="outline"
+                      >
+                        Contact Me
+                      </Button>
+                    </motion.div>
+                  </Stack>
                 </Box>
               </Box>
             </FadeIn>
--- theme/index.ts
+++ theme/index.ts
@@ -1,9 +1,46 @@
 import {
   extendTheme,
+  StyleConfig,
   StyleFunctionProps,
   type ThemeConfig
 } from '@chakra-ui/react';
 import { mode } from '@chakra-ui/theme-tools';
+
+const colors = {
+  primary: {
+    100: '#B3E5FC',
+    200: '#81D4FA',
+    300: '#4FC3F7',
+    400: '#29B6F6',
+    500: '#0293D5',
+    600: '#0077B6',
+    700: '#005691',
+    800: '#003366',
+    900: '#001A29'
+  }
+};
+
+const components: Record<string, StyleConfig> = {
+  Button: {
+    variants: {
+      outline: (props) => ({
+        borderColor: 'primary.500',
+        color: 'primary.500',
+        _hover: {
+          backgroundColor: props.colorMode === 'dark' ? 'gray.700' : 'gray.100',
+          color: props.colorMode === 'dark' ? 'white' : 'primary.500'
+        }
+      }),
+      solid: {
+        backgroundColor: 'primary.500',
+        color: 'white',
+        _hover: {
+          backgroundColor: 'primary.700'
+        }
+      }
+    }
+  }
+};
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
@@ -19,6 +56,6 @@ const styles = {
   })
 };
 
-const theme = extendTheme({ config, styles });
+const theme = extendTheme({ colors, components, config, styles });
 
 export default theme;
