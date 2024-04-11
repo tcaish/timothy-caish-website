a9b81c2f64c22bfe2ee2df4adac4d76e453ee213
tcaish <77754475+tcaish@users.noreply.github.com>
2024-04-11 16:06:05 -0400

    add mobile nav drawer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import AnimatedPressIn from '@/components/AnimatedPressIn';
 import LanguagesMenu from '@/components/LanguagesMenu';
+import NavigationDrawer from '@/components/Navigation/Drawer';
 import { Routes } from '@/constants/routes';
 import { i18n } from '@/services/localization';
 import { useStore } from '@/zustand/store';
@@ -9,18 +10,26 @@ import { useColorMode } from '@chakra-ui/color-mode';
 import { Image } from '@chakra-ui/image';
 import { Box, Container, Divider, Flex, Spacer } from '@chakra-ui/layout';
 import { Link } from '@chakra-ui/next-js';
-import { Icon, Text, Tooltip, useColorModeValue } from '@chakra-ui/react';
+import {
+  Icon,
+  Text,
+  Tooltip,
+  useColorModeValue,
+  useDisclosure
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { useTheme } from 'next-themes';
 import React from 'react';
 import { BsMoonFill } from 'react-icons/bs';
 import { FaSun } from 'react-icons/fa';
+import { TbMenuDeep } from 'react-icons/tb';
 
 /**
  * Navigation bar component.
  */
 export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const mobileNavbarDisclosure = useDisclosure();
   const store = useStore();
   const { theme, setTheme } = useTheme();
 
@@ -45,110 +54,132 @@ export default function Navbar() {
   }
 
   return (
-    <nav>
-      <Container maxW="container.xl" paddingY={2}>
-        <Flex alignItems="center">
-          {/* Left side - logo */}
-          <Box>
-            <Link href={Routes.Home.path}>
-              <Image
-                alt="Timothy Caish Logo"
-                src={useColorModeValue(
-                  '/assets/logo/logo__light.png',
-                  '/assets/logo/logo__dark.png'
-                )}
-                w={{ base: '100px', md: '150px' }}
-              />
-            </Link>
-          </Box>
+    <>
+      <nav>
+        <Container maxW="container.xl" paddingY={2}>
+          <Flex alignItems="center">
+            {/* Left side - logo */}
+            <Box>
+              <Link href={Routes.Home.path}>
+                <Image
+                  alt="Timothy Caish Logo"
+                  src={useColorModeValue(
+                    '/assets/logo/logo__light.png',
+                    '/assets/logo/logo__dark.png'
+                  )}
+                  w={{ base: '100px', md: '150px' }}
+                />
+              </Link>
+            </Box>
 
-          <Spacer />
+            <Spacer />
 
-          {/* Middle - links */}
-          <Box display={{ base: 'none', lg: 'flex' }}>
-            <Flex alignItems="center" me={4} gap={4}>
-              {Object.values(Routes)
-                .filter((r) => r.name !== Routes.Home.name)
-                .map((route) => {
-                  return route.name === Routes.Contact.name ||
-                    route.name === Routes.Portfolio.name ? (
-                    <Tooltip
-                      bg={colorMode === 'light' ? 'gray.200' : 'gray.600'} // Using colorMode here because we will get React hooks error if we use useColorModeValue
-                      color={colorMode === 'light' ? 'gray.700' : 'white'} // Using colorMode here because we will get React hooks error if we use useColorModeValue
-                      hasArrow={true}
-                      key={route.name}
-                      label={inDevText}
-                      placement="bottom"
-                    >
-                      <Text
-                        color={useColorModeValue('gray.300', 'gray.600')}
-                        fontWeight="semibold"
+            {/* Middle - links */}
+            <Box display={{ base: 'none', lg: 'flex' }}>
+              <Flex alignItems="center" me={4} gap={4}>
+                {Object.values(Routes)
+                  .filter((r) => r.name !== Routes.Home.name)
+                  .map((route) => {
+                    return route.name === Routes.Contact.name ||
+                      route.name === Routes.Portfolio.name ? (
+                      <Tooltip
+                        bg={colorMode === 'light' ? 'gray.200' : 'gray.600'} // Using colorMode here because we will get React hooks error if we use useColorModeValue
+                        color={colorMode === 'light' ? 'gray.700' : 'white'} // Using colorMode here because we will get React hooks error if we use useColorModeValue
+                        hasArrow={true}
+                        key={route.name}
+                        label={inDevText}
+                        placement="bottom"
                       >
-                        {route.name}
-                      </Text>
-                    </Tooltip>
-                  ) : (
-                    <Link href={route.path} key={route.name}>
-                      <AnimatedPressIn>
                         <Text
-                          color={
-                            route.name === Routes.Contact.name ||
-                            route.name === Routes.Portfolio.name
-                              ? useColorModeValue('gray.300', 'gray.600')
-                              : useColorModeValue('gray.700', 'gray.300')
-                          }
+                          color={useColorModeValue('gray.300', 'gray.600')}
                           fontWeight="semibold"
                         >
                           {route.name}
                         </Text>
-                      </AnimatedPressIn>
-                    </Link>
-                  );
-                })}
-            </Flex>
-          </Box>
-
-          <Spacer />
-
-          {/* Right side */}
-          <Flex>
-            {/* Languages menu */}
-            <Box
-              me={4}
-              _hover={{
-                cursor: 'pointer'
-              }}
-            >
-              <LanguagesMenu />
+                      </Tooltip>
+                    ) : (
+                      <Link href={route.path} key={route.name}>
+                        <AnimatedPressIn>
+                          <Text
+                            color={
+                              route.name === Routes.Contact.name ||
+                              route.name === Routes.Portfolio.name
+                                ? useColorModeValue('gray.300', 'gray.600')
+                                : useColorModeValue('gray.700', 'gray.300')
+                            }
+                            fontWeight="semibold"
+                          >
+                            {route.name}
+                          </Text>
+                        </AnimatedPressIn>
+                      </Link>
+                    );
+                  })}
+              </Flex>
             </Box>
 
-            {/* Theme toggle */}
-            <Box
-              _hover={{
-                cursor: 'pointer'
-              }}
-            >
-              <motion.div
-                onClick={handleChangeColorMode}
-                style={{ maxHeight: '2rem' }}
-                whileHover={{ scale: 1.2, rotate: 90 }}
-                whileTap={{
-                  scale: 0.8,
-                  rotate: -90
+            <Spacer />
+
+            {/* Right side */}
+            <Flex>
+              {/* Languages menu */}
+              <Box
+                me={4}
+                _hover={{
+                  cursor: 'pointer'
                 }}
               >
-                {colorMode === 'dark' ? (
-                  <Icon as={FaSun} color="#DBC300" boxSize={8} />
-                ) : (
-                  <Icon as={BsMoonFill} color="#2D3748" boxSize={7} />
-                )}
-              </motion.div>
-            </Box>
-          </Flex>
-        </Flex>
-      </Container>
+                <LanguagesMenu />
+              </Box>
 
-      <Divider borderColor={useColorModeValue('gray.200', 'gray.600')} />
-    </nav>
+              {/* Theme toggle */}
+              <Box
+                me={4}
+                _hover={{
+                  cursor: 'pointer'
+                }}
+              >
+                <motion.div
+                  onClick={handleChangeColorMode}
+                  style={{ maxHeight: '2rem' }}
+                  whileHover={{ scale: 1.2, rotate: 90 }}
+                  whileTap={{
+                    scale: 0.8,
+                    rotate: -90
+                  }}
+                >
+                  {colorMode === 'dark' ? (
+                    <Icon as={FaSun} color="#DBC300" boxSize={8} />
+                  ) : (
+                    <Icon as={BsMoonFill} color="#2D3748" boxSize={7} />
+                  )}
+                </motion.div>
+              </Box>
+
+              {/* Mobile nav menu */}
+              <Box
+                display={{ base: 'flex', lg: 'none' }}
+                _hover={{ cursor: 'pointer' }}
+              >
+                <motion.div
+                  onClick={mobileNavbarDisclosure.onOpen}
+                  whileHover={{ scale: 1.2 }}
+                  whileTap={{ scale: 0.8 }}
+                >
+                  <TbMenuDeep size={35} />
+                </motion.div>
+              </Box>
+            </Flex>
+          </Flex>
+        </Container>
+
+        <Divider borderColor={useColorModeValue('gray.200', 'gray.600')} />
+      </nav>
+
+      <NavigationDrawer
+        isOpen={mobileNavbarDisclosure.isOpen}
+        onClose={mobileNavbarDisclosure.onClose}
+      />
+    </>
   );
 }
---
+++ components/Navigation/Drawer.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import AnimatedPressIn from '@/components/AnimatedPressIn';
+import { Routes } from '@/constants/routes';
+import { i18n } from '@/services/localization';
+import { Link } from '@chakra-ui/next-js';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  Tooltip,
+  useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react';
+
+type NavigationDrawerProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export default function NavigationDrawer(props: NavigationDrawerProps) {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Drawer
+      isOpen={props.isOpen}
+      onClose={props.onClose}
+      placement="right"
+      size="xs"
+    >
+      <DrawerOverlay />
+
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader />
+
+        <DrawerBody>
+          <Flex alignItems="center" direction="column" me={4} gap={4}>
+            {Object.values(Routes).map((route) => {
+              return route.name === Routes.Contact.name ||
+                route.name === Routes.Portfolio.name ? (
+                <Tooltip
+                  bg={colorMode === 'light' ? 'gray.200' : 'gray.600'} // Using colorMode here because we will get React hooks error if we use useColorModeValue
+                  color={colorMode === 'light' ? 'gray.700' : 'white'} // Using colorMode here because we will get React hooks error if we use useColorModeValue
+                  hasArrow={true}
+                  key={route.name}
+                  label={i18n.t('in_development')}
+                  placement="left"
+                >
+                  <Heading
+                    color={useColorModeValue('gray.300', 'gray.600')}
+                    fontWeight="semibold"
+                    size="lg"
+                  >
+                    {route.name}
+                  </Heading>
+                </Tooltip>
+              ) : (
+                <Link
+                  href={route.path}
+                  key={route.name}
+                  onClick={props.onClose}
+                >
+                  <AnimatedPressIn>
+                    <Heading
+                      color={
+                        route.name === Routes.Contact.name ||
+                        route.name === Routes.Portfolio.name
+                          ? useColorModeValue('gray.300', 'gray.600')
+                          : useColorModeValue('gray.700', 'gray.300')
+                      }
+                      fontWeight="semibold"
+                      size="lg"
+                    >
+                      {route.name}
+                    </Heading>
+                  </AnimatedPressIn>
+                </Link>
+              );
+            })}
+          </Flex>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+}
