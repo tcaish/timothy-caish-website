dcb21b37f73b7f689158013d76a0ffff67f5b8af
tcaish <77754475+tcaish@users.noreply.github.com>
2024-05-23 07:58:57 -0400

    fetch and display portfolio items

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,12 +1,34 @@
 'use client';
 
 import PageContainer from '@/components/PageContainer';
-import { SimpleGrid } from '@chakra-ui/react';
+import PortfolioCard from '@/components/PortfolioCard';
+import { Tables } from '@/constants/types/supabase';
+import { getPortfolioItems } from '@/services/supabase-database/getters/portfolio_items';
+import { Center, SimpleGrid } from '@chakra-ui/react';
+import React from 'react';
 
-export default function Contact() {
+export default function Portfolio() {
+  const [portfolioItems, setPortfolioItems] = React.useState<
+    Tables<'portfolio_items'>[]
+  >([]);
+
+  // Fetch the portfolio items
+  React.useEffect(() => {
+    (async () => {
+      const response = await getPortfolioItems();
+      setPortfolioItems(response);
+    })();
+  }, []);
+
   return (
     <PageContainer>
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}></SimpleGrid>
+      <Center>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} px={4} spacing={4}>
+          {portfolioItems.map((portfolioItem) => (
+            <PortfolioCard {...portfolioItem} />
+          ))}
+        </SimpleGrid>
+      </Center>
     </PageContainer>
   );
 }
