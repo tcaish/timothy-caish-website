85c35f59cc8c7640952cd54f475ded2926613fe8
tcaish <77754475+tcaish@users.noreply.github.com>
2024-05-27 16:30:56 -0400

    added portfolio item comments table types

--- constants/types/supabase.ts
+++ constants/types/supabase.ts
@@ -4,119 +4,150 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   public: {
     Tables: {
+      portfolio_item_comments: {
+        Row: {
+          approved: boolean | null
+          comment: string
+          created_at: string
+          id: number
+          name: string | null
+          portfolio_item_id: number
+        }
+        Insert: {
+          approved?: boolean | null
+          comment: string
+          created_at?: string
+          id?: number
+          name?: string | null
+          portfolio_item_id: number
+        }
+        Update: {
+          approved?: boolean | null
+          comment?: string
+          created_at?: string
+          id?: number
+          name?: string | null
+          portfolio_item_id?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "portfolio_item_comments_portfolio_item_id_fkey"
+            columns: ["portfolio_item_id"]
+            isOneToOne: false
+            referencedRelation: "portfolio_items"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       portfolio_items: {
         Row: {
-          created_at: string;
-          id: number;
-          image_url: string;
-          learn_more_url: string | null;
-          release_date: string | null;
-          title: string;
-          total_likes: number;
-          type: string;
-        };
+          created_at: string
+          id: number
+          image_url: string
+          learn_more_url: string | null
+          release_date: string | null
+          title: string
+          total_likes: number
+          type: string
+        }
         Insert: {
-          created_at?: string;
-          id?: number;
-          image_url: string;
-          learn_more_url?: string | null;
-          release_date?: string | null;
-          title: string;
-          total_likes?: number;
-          type: string;
-        };
+          created_at?: string
+          id?: number
+          image_url: string
+          learn_more_url?: string | null
+          release_date?: string | null
+          title: string
+          total_likes?: number
+          type: string
+        }
         Update: {
-          created_at?: string;
-          id?: number;
-          image_url?: string;
-          learn_more_url?: string | null;
-          release_date?: string | null;
-          title?: string;
-          total_likes?: number;
-          type?: string;
-        };
-        Relationships: [];
-      };
+          created_at?: string
+          id?: number
+          image_url?: string
+          learn_more_url?: string | null
+          release_date?: string | null
+          title?: string
+          total_likes?: number
+          type?: string
+        }
+        Relationships: []
+      }
       unique_visitors: {
         Row: {
-          created_at: string;
-          hashed_ipv6: string;
-          id: number;
-        };
+          created_at: string
+          hashed_ipv6: string
+          id: number
+        }
         Insert: {
-          created_at?: string;
-          hashed_ipv6: string;
-          id?: number;
-        };
+          created_at?: string
+          hashed_ipv6: string
+          id?: number
+        }
         Update: {
-          created_at?: string;
-          hashed_ipv6?: string;
-          id?: number;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string
+          hashed_ipv6?: string
+          id?: number
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       decrement_portfolio_item_likes: {
         Args: {
-          id_of_portfolio_item: number;
-        };
-        Returns: undefined;
-      };
+          id_of_portfolio_item: number
+        }
+        Returns: undefined
+      }
       increment_portfolio_item_likes: {
         Args: {
-          id_of_portfolio_item: number;
-        };
-        Returns: undefined;
-      };
-    };
+          id_of_portfolio_item: number
+        }
+        Returns: undefined
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type PublicSchema = Database[Extract<keyof Database, "public">];
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
     | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (
-      & Database[PublicTableNameOrOptions["schema"]]["Tables"]
-      & Database[PublicTableNameOrOptions["schema"]]["Views"]
-    )
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
     : never = never,
-> = PublicTableNameOrOptions extends { schema: keyof Database } ? (
-    & Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    & Database[PublicTableNameOrOptions["schema"]]["Views"]
-  )[TableName] extends {
-    Row: infer R;
-  } ? R
-  : never
-  : PublicTableNameOrOptions extends keyof (
-    & PublicSchema["Tables"]
-    & PublicSchema["Views"]
-  ) ? (
-      & PublicSchema["Tables"]
-      & PublicSchema["Views"]
-    )[PublicTableNameOrOptions] extends {
-      Row: infer R;
-    } ? R
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
     : never
-  : never;
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -127,15 +158,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-    Insert: infer I;
-  } ? I
-  : never
+      Insert: infer I
+    }
+    ? I
+    : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I;
-    } ? I
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -146,15 +179,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-    Update: infer U;
-  } ? U
-  : never
+      Update: infer U
+    }
+    ? U
+    : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U;
-    } ? U
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -167,4 +202,4 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-  : never;
+    : never
