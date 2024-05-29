f8cd71df8f5c958775d9c5336249ecd3abe400f2
tcaish <77754475+tcaish@users.noreply.github.com>
2024-05-29 09:49:17 -0400

    fix confetti moving cards around

--- components/animation/ConfettiExplosion.tsx
+++ components/animation/ConfettiExplosion.tsx
@@ -1,4 +1,3 @@
-import { MotionBox } from '@/components/animation/MotionBox';
 import React from 'react';
 import Confetti from 'react-confetti';
 
@@ -14,27 +13,18 @@ export default function ConfettiExplosion(props: ConfettiExplosionProps) {
   const confettiRef = React.useRef(null);
 
   return (
-    <MotionBox
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 0.1 }}
-    >
-      <Confetti
-        confettiSource={{
-          h: props.height,
-          w: props.width,
-          x: props.x,
-          y: props.y
-        }}
-        gravity={0.2}
-        height={window.innerHeight}
-        numberOfPieces={30}
-        onConfettiComplete={props.onConfettiComplete}
-        recycle={false}
-        ref={confettiRef}
-        width={window.innerWidth}
-      />
-    </MotionBox>
+    <Confetti
+      confettiSource={{
+        h: props.height,
+        w: props.width,
+        x: props.x,
+        y: props.y
+      }}
+      gravity={0.2}
+      numberOfPieces={30}
+      onConfettiComplete={props.onConfettiComplete}
+      recycle={false}
+      ref={confettiRef}
+    />
   );
 }
