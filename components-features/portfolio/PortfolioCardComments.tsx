b3a1e0ababed1f684c1c76f7abcd8402b9f91522
tcaish <77754475+tcaish@users.noreply.github.com>
2024-05-29 08:44:58 -0400

    be able to load additional comments

--- components-features/portfolio/PortfolioCardComments.tsx
+++ components-features/portfolio/PortfolioCardComments.tsx
@@ -1,11 +1,12 @@
 import GhostJson from '@/assets/lottie/ghost.json';
 import PortfolioCardComment from '@/components-features/portfolio/PortfolioCardComment';
 import PortfolioCardCommentSkeleton from '@/components-features/portfolio/PortfolioCardCommentSkeleton';
+import { CONTENT_SIZE_LIMIT } from '@/constants/settings';
 import { Tables } from '@/constants/types/supabase';
 import { i18n } from '@/services/localization';
 import { getPortfolioItemComments } from '@/services/supabase-database/getters/portfolio_item_comments';
 import { useStore } from '@/zustand/store';
-import { Stack, Text, useToast } from '@chakra-ui/react';
+import { Button, Flex, Stack, Text, useToast } from '@chakra-ui/react';
 import Lottie from 'lottie-react';
 import React from 'react';
 
@@ -17,33 +18,12 @@ export default function PortfolioCardComments() {
     Tables<'portfolio_item_comments'>[]
   >([]);
   const [isLoading, setIsLoading] = React.useState(false);
+  const [isLoadingMoreComments, setIsLoadingMoreComments] =
+    React.useState(false);
 
   // Get the comments for the portfolio item
   React.useEffect(() => {
-    (async () => {
-      if (!store.portfolioItemIdSelected) return;
-
-      setIsLoading(true);
-
-      // Get the comments for the portfolio item
-      const response = await getPortfolioItemComments(
-        store.portfolioItemIdSelected
-      );
-
-      // If there was an error loading the comments
-      if (response.error) {
-        setIsLoading(false);
-        toast({
-          description: i18n.t('error__loading_comments__desc'),
-          isClosable: true,
-          status: 'error'
-        });
-        return;
-      }
-
-      setComments(response.comments ?? []);
-      setIsLoading(false);
-    })();
+    fetchComments();
   }, []);
 
   /**
@@ -66,6 +46,52 @@ export default function PortfolioCardComments() {
     );
   }
 
+  /**
+   * Fetches the portfolio item comments.
+   * @param fetchAdditional Whether or not to fetch the next set of comments.
+   */
+  async function fetchComments(fetchAdditional = false) {
+    if (!store.portfolioItemIdSelected) return;
+
+    fetchAdditional ? setIsLoadingMoreComments(true) : setIsLoading(true);
+
+    // Get the comments for the portfolio item
+    const response = await getPortfolioItemComments(
+      store.portfolioItemIdSelected,
+      fetchAdditional
+    );
+
+    // If there was an error loading the comments
+    if (response.error) {
+      fetchAdditional ? setIsLoadingMoreComments(false) : setIsLoading(false);
+      toast({
+        description: i18n.t('error__loading_comments__desc'),
+        isClosable: true,
+        status: 'error'
+      });
+      return;
+    }
+
+    let tempComments: Tables<'portfolio_item_comments'>[] = [];
+
+    // If there are comments
+    if (response.comments) {
+      tempComments = fetchAdditional
+        ? [...comments, ...response.comments]
+        : response.comments;
+    }
+
+    // Sort ascending based on the created_at date
+    tempComments.sort((a, b) => {
+      return (
+        new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+      );
+    });
+
+    setComments(tempComments);
+    fetchAdditional ? setIsLoadingMoreComments(false) : setIsLoading(false);
+  }
+
   return (
     <React.Fragment>
       {isLoading ? (
@@ -80,8 +106,24 @@ export default function PortfolioCardComments() {
             <EmptyComments />
           ) : (
             <Stack spacing={3}>
-              {comments.map((comment) => (
-                <PortfolioCardComment key={comment.id} {...comment} />
+              {comments.map((comment, index) => (
+                <React.Fragment>
+                  <PortfolioCardComment key={comment.id} {...comment} />
+
+                  {index === comments.length - 1 &&
+                    comments.length % CONTENT_SIZE_LIMIT === 0 && (
+                      <Flex justifyContent="center">
+                        <Button
+                          colorScheme="primary"
+                          isLoading={isLoadingMoreComments}
+                          onClick={() => fetchComments(true)}
+                          variant="link"
+                        >
+                          Load more comments
+                        </Button>
+                      </Flex>
+                    )}
+                </React.Fragment>
               ))}
             </Stack>
           )}
